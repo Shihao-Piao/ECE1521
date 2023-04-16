a8006e902e5c0b33067a353223f2fccc511d4f46
Shihao-Piao <2359282910@qq.com>
2023-04-15 20:39:38 -0400

    Update wiener.py

--- wiener.py
+++ wiener.py
@@ -10,27 +10,27 @@ def blur(img,kernel_size = 3):
     return dummy
 
 def add_gaussian_noise(img, sigma):
-	gauss = np.random.normal(0, sigma, np.shape(img))
-	noisy_img = img + gauss
-	noisy_img[noisy_img < 0] = 0
-	noisy_img[noisy_img > 255] = 255
-	return noisy_img
+    gauss = np.random.normal(0, sigma, np.shape(img))
+    noisy_img = img + gauss
+    noisy_img[noisy_img < 0] = 0
+    noisy_img[noisy_img > 255] = 255
+    return noisy_img
 
 def wiener_filter(img, kernel, K):
-	kernel /= np.sum(kernel)
-	dummy = np.copy(img)
-	dummy = fft2(dummy)
-	kernel = fft2(kernel, s = img.shape)
-	kernel = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
-	dummy = dummy * kernel
-	dummy = np.abs(ifft2(dummy))
-	return dummy
+    kernel /= np.sum(kernel)
+    dummy = np.copy(img)
+    dummy = fft2(dummy)
+    kernel = fft2(kernel, s = img.shape)
+    kernel = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
+    dummy = dummy * kernel
+    dummy = np.abs(ifft2(dummy))
+    return dummy
 
 def gaussian_kernel(kernel_size = 3):
-	h = gaussian(kernel_size, kernel_size / 3).reshape(kernel_size, 1)
-	h = np.dot(h, h.transpose())
-	h /= np.sum(h)
-	return h
+    h = gaussian(kernel_size, kernel_size / 3).reshape(kernel_size, 1)
+    h = np.dot(h, h.transpose())
+    h /= np.sum(h)
+    return h
 
 def rgb2gray(rgb):
-	return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
