a5810c7998187489736422098105c8dbab7e2700
Shihao-Piao <2359282910@qq.com>
2023-04-16 00:36:13 -0400

    wiener

--- test.py
+++ test.py
@@ -1,5 +1,90 @@
+
+'''
+#wiener filter with average K
 import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import skimage
+
+# Defining Values
+
+blur = 5
+variance = 1000
+
+def readimage(string):
+    image = cv2.imread(string,0)
+    imagefinal = cv2.resize(image, (512,512))
+    return imagefinal
+
+def showimage(image):
+    plot = plt.imshow(image,cmap='gray')
+    plt.show(plot)
+    return plot
+
+def add_blur(image):
+    imgblur = cv2.blur(image,(blur,blur))
+    return imgblur
+
+def add_noise(image):
+    mean=0
+    sigma = variance**0.5
+    row,col= image.shape
+    gauss = np.random.normal(mean,sigma,(row,col))
+    gauss = gauss.reshape(row,col)
+    noisy_image = np.uint8(image + gauss)
+    return noisy_image
+
+def psnr(img1,img2):
+    return skimage.measure.compare_psnr(img1,img2)
 
 
-print("hello world？")
-print("hello world")
+def psd(image):
+    fourierimage = np.fft.fft2(image)
+    psdimage = fourierimage * np.conj(fourierimage) / (512 * 512)
+    return psdimage
+
+
+def wiener_filter():
+    h = np.ones((blur, blur)) / (blur * blur)
+    H = np.fft.fft2(h, [512, 512])
+    psdmean = 0
+    for i in glob.glob("*.png"):
+        grayimage = cv2.imread(i, 0)
+        image = cv2.resize(grayimage, (512, 512))
+        img_blur = add_blur(image)
+        noisy = add_noise(img_blur)
+
+        PSD = psd(image)
+        psdmean += PSD
+    psdmean = psdmean / 14
+    N = variance * np.ones((512, 512))
+    K = N / psdmean
+    print(K)
+    Filter = np.conj(H) / (H * np.conj(H) + K)
+
+    return Filter
+
+
+Filter = wiener_filter()
+
+image=readimage('lena.png')
+plt.imshow(image)
+plt.show()
+
+img_blur=add_blur(image)
+noise = add_noise(img_blur)
+plt.imshow(noise,cmap='gray')
+plt.show()
+
+def filter_test(noisy_image,wiener_filter):
+    f=wiener_filter*np.fft.fft2(noisy_image)
+    o=np.fft.ifft2(f)
+    output=np.uint8(o)
+    return output
+result = filter_test(noise,Filter)
+plt.imshow(result,cmap='gray')
+plt.show()
+
+
+'''
--- wiener.py
+++ wiener.py
@@ -1,13 +1,20 @@
+import os
 import numpy as np
 from numpy.fft import fft2, ifft2
 from scipy.signal import gaussian, convolve2d
 import matplotlib.pyplot as plt
+import cv2
 
-def blur(img,kernel_size = 3):
-    dummy = np.copy(img)
-    h = np.eye(kernel_size) / kernel_size
-    dummy = convolve2d(dummy, h, mode='valid')
-    return dummy
+def readimage(string):
+    image = cv2.imread(string, 0)
+    imagefinal = cv2.resize(image, (512, 512))
+    return imagefinal
+
+
+def blur(img, kernel_size=3):
+    imgblur = cv2.blur(img, (kernel_size, kernel_size))
+    return imgblur
+
 
 def add_gaussian_noise(img, sigma):
     gauss = np.random.normal(0, sigma, np.shape(img))
@@ -16,21 +23,51 @@ def add_gaussian_noise(img, sigma):
     noisy_img[noisy_img > 255] = 255
     return noisy_img
 
+
 def wiener_filter(img, kernel, K):
     kernel /= np.sum(kernel)
     dummy = np.copy(img)
     dummy = fft2(dummy)
-    kernel = fft2(kernel, s = img.shape)
+    kernel = fft2(kernel, s=img.shape)
     kernel = np.conj(kernel) / (np.abs(kernel) ** 2 + K)
     dummy = dummy * kernel
     dummy = np.abs(ifft2(dummy))
     return dummy
 
-def gaussian_kernel(kernel_size = 3):
+
+def gaussian_kernel(kernel_size=3):
     h = gaussian(kernel_size, kernel_size / 3).reshape(kernel_size, 1)
     h = np.dot(h, h.transpose())
     h /= np.sum(h)
     return h
 
+
 def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+if __name__ == '__main__':
+    # Load image and convert it to gray scale
+    img = readimage('lena.png')
+
+    # Blur the image
+    blurred_img = blur(img, kernel_size=7)
+
+    # Add Gaussian noise
+    noisy_img = add_gaussian_noise(blurred_img, sigma=30)
+
+    # Apply Wiener Filter
+    kernel = gaussian_kernel(5)
+    filtered_img = wiener_filter(noisy_img, kernel, K=10)
+    # Display results
+    display = [img, blurred_img, noisy_img, filtered_img]
+    label = ['Original Image', 'Motion Blurred Image', 'Motion Blurring + Gaussian Noise', 'Wiener Filter applied']
+
+    fig = plt.figure(figsize=(12, 10))
+
+    for i in range(len(display)):
+        fig.add_subplot(2, 2, i + 1)
+        plt.imshow(display[i], cmap='gray')
+        plt.title(label[i])
+
+    plt.show()
